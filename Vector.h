a9def7b14fea7241be09983aa94a37deffeee490
Damyan Boychev <dboychev@astea.net>
2020-08-10 13:39:55 +0300

    Add return values and clean code

--- Vector.h
+++ Vector.h
@@ -3,43 +3,39 @@
 
 template<typename T>
 class Vector {
+private:
     T* data;
     std::size_t size, capacity;
 
+public:
     Vector();
 
-    void push_back(const T& el);
-    void pop_back();
+    Vector& push_back(const T& el);
+    Vector& pop_back();
 };
 
 template<typename T>
-Vector::Vector() {
+Vector<T>::Vector() {
     this->size = 0;
     this->capacity = 8;
     this->data = new T[this->capacity];
 }
 
 template<typename T>
-void Vector::push_back(const T& el) {
+Vector<T>& Vector<T>::push_back(const T& el) {
     this->data[size] = el;
 	this->size++;
+
+    return *this;
 }
 
 template<typename T>
-void Vector::pop_back()
+Vector<T>& Vector<T>::pop_back()
 {
-    if(this->size <= 0)
-    {
-        return;
-    } 
-
-    T* smaller_vector = new T[capacity];
-    for(size_t i = 0; i < this->size - 1, i++)
-    {
-        smaller_vector[i] = this->data[i];
+    if(this->size <= 0) {
+        return *this;
     }
-    
-    delete[] this->vector;
-    this->vector = smaller_vector;
     this->size--;
+
+    return *this;
 }
