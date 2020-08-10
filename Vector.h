bd39f94e4a241322d37091fbd012e689b0b805db
Damyan Boychev <dboychev@astea.net>
2020-08-10 10:42:36 +0300

    Add constructor

--- Vector.h
+++ Vector.h
@@ -5,4 +5,13 @@ template<typename T>
 class Vector {
     T* data;
     std::size_t size, capacity;
+
+    Vector();
 };
+
+template<typename T>
+Vector::Vector() {
+    this->size = 0;
+    this->capacity = 8;
+    this->data = new T[this->capacity];
+}
